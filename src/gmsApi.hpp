1aafa04fe948b948edef8852c33524d036516f29
David Nickerson <nickerso@users.sourceforge.net>
2013-01-14 17:22:52 +1300

    adding some notes on the webservice API that I want to work toward

--- src/gmsApi.hpp
+++ src/gmsApi.hpp
@@ -1,6 +1,22 @@
 #ifndef GMSAPI_HPP
 #define GMSAPI_HPP
 
+/**
+  * GMS webservice ideas.
+  *
+  * /model/ - returns listing of all available models (model == workspace)
+  * /model/id - returns basic information about the model
+  * /model/id/math - maybe return a presentation of the model math?
+  *
+  * /model/id/sed - return a listing of all SED's for the model (maybe part of the basic information above?)
+  * /model/id/sed/output/id - return the specified output (chart, report, etc)
+  *
+  *
+  * /query | /search - perform requested search on the database and return results as above?
+  * e.g., /query?species=rat
+  *       /query?species=rat&organ=kidney
+  */
+
 namespace GMS
 {
     class Data;
