f4dbd74c3efb9144f0d95bbd501d2fd3d37db9f1
David Nickerson <nickerso@users.noreply.github.com>
2013-01-14 22:06:56 +1300

    parsing the model listing file

--- src/gmsData.cpp
+++ src/gmsData.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <vector>
+#include <sstream>
 
 #include "gmsData.hpp"
 
 using namespace GMS;
+
+static std::vector<std::string>&
+splitString(const std::string &s, char delim, std::vector<std::string>& elems)
+{
+    std::stringstream ss(s);
+    std::string item;
+    while(std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
+    return elems;
+}
 
 static size_t retrieveContent(char* buffer, size_t size, size_t nmemb, void* string)
 {
@@ -41,11 +54,11 @@ int Data::initialiseModelDatabase(const std::string &repositoryRoot)
     }
     mRepositoryRoot = repositoryRoot;
     std::string workspacesUrl = repositoryRoot + "workspaces";
-    std::string* data = new std::string();
+    std::string data;
     CURL* curl = static_cast<CURL*>(mCurl);
     curl_easy_setopt(curl, CURLOPT_URL, workspacesUrl.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, retrieveContent);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(data));
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(&data));
     CURLcode res = curl_easy_perform(curl);
     if(CURLE_OK != res)
     {
@@ -53,7 +66,11 @@ int Data::initialiseModelDatabase(const std::string &repositoryRoot)
         fprintf(stderr, "curl told us %d\n", res);
         return -1;
     }
-    std::cout << "Model Listing:\n**" << data->c_str() << "**\n";
-    delete data;
+    mModelList = splitString(data, '\n', mModelList);
+    /*std::cout << "Model Listing:\n";
+    for (int i = 0; i < mModelList.size(); ++i)
+    {
+        std::cout << "**" << mModelList[i] << "**\n";
+    }*/
     return code;
 }
