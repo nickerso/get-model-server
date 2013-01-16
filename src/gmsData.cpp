8df487d9f857bc4a8e35cf3dff3f9232875896f9
David Nickerson <nickerso@users.noreply.github.com>
2013-01-16 17:13:13 +1300

    starting to load and process manifest files; only accepting workspaces which have a manifest file; trying to keep all libxml encapsulated in the XmlDoc class.

--- src/gmsData.cpp
+++ src/gmsData.cpp
@@ -1,73 +1,52 @@
 #include <iostream>
-#include <curl/curl.h>
 #include <vector>
-#include <sstream>
+#include <algorithm>
 #include <sedml/reader.h>
 
 #include "gmsData.hpp"
+#include "workspace.hpp"
+#include "utils.hpp"
 
 using namespace GMS;
 
-static std::vector<std::string>&
-splitString(const std::string &s, char delim, std::vector<std::string>& elems)
+class WorkspaceLoader
 {
-    std::stringstream ss(s);
-    std::string item;
-    while(std::getline(ss, item, delim))
+public:
+    WorkspaceLoader(Data* dataHandle, const std::string& repositoryRoot) : data(dataHandle), root(repositoryRoot)
     {
-        elems.push_back(item);
     }
-    return elems;
-}
+    // used with for_each
+    void operator()(const std::string& workspaceName)
+    {
+        std::string url = root + workspaceName;
+        std::cout << "Loading workspace: \'" << url.c_str() << "\'\n";
+        Workspace* workspace = new Workspace(url);
+        data->addWorkspace(workspace);
+    }
 
-static size_t retrieveContent(char* buffer, size_t size, size_t nmemb, void* string)
-{
-    std::string* s = static_cast<std::string*>(string);
-    size_t bytes = size * nmemb;
-    s->append(buffer, bytes);
-    return bytes;
-}
+    Data* data;
+    const std::string& root;
+};
 
 Data::Data()
 {
     std::cout << "Creating new GMS::Data for use in the GET model server." << std::endl;
-    CURL* curl = curl_easy_init();
-    mCurl = static_cast<void*>(curl);
 }
 
 Data::~Data()
 {
     std::cout << "Destroying the GMS::Data from the GET model server." << std::endl;
-    if (mCurl)
-    {
-        CURL* curl = static_cast<CURL*>(mCurl);
-        curl_easy_cleanup(curl);
-    }
 }
 
 int Data::initialiseModelDatabase(const std::string &repositoryRoot)
 {
     int code = 0;
-    if (!mCurl)
-    {
-        std::cerr << "Missing CURL handle?\n";
-        return -2;
-    }
     mRepositoryRoot = repositoryRoot;
     std::string workspacesUrl = repositoryRoot + "workspaces";
-    std::string data;
-    CURL* curl = static_cast<CURL*>(mCurl);
-    curl_easy_setopt(curl, CURLOPT_URL, workspacesUrl.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, retrieveContent);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, static_cast<void*>(&data));
-    CURLcode res = curl_easy_perform(curl);
-    if(CURLE_OK != res)
-    {
-        /* we failed */
-        fprintf(stderr, "curl told us %d\n", res);
-        return -1;
-    }
+    std::string data = getUrlContent(workspacesUrl);
     mModelList = splitString(data, '\n', mModelList);
+    WorkspaceLoader loader(this, mRepositoryRoot);
+    for_each(mModelList.begin(), mModelList.end(), loader);
 
     struct sedml_document* doc = sedml_create_document();
     int r = sedml_read_file("http://models.cellml.org/w/andre/sine/rawfile/293afb20feb51d1739b6645eaf2cd18b1a4f3bcb/sin_approximations_sedml.xml", NULL, doc);
@@ -94,4 +73,10 @@ int Data::initialiseModelDatabase(const std::string &repositoryRoot)
         std::cout << "**" << mModelList[i] << "**\n";
     }*/
     return code;
+}
+
+int Data::addWorkspace(Workspace *workspace)
+{
+    mWorkspaces.push_back(workspace);
+    return 0;
 }
