cfecab7051a8e8c276d9c683636bf4790e4c9680
David Nickerson <nickerso@users.sourceforge.net>
2013-04-25 17:16:27 -0500

    playing around with ways to resolve URI's; deciding that since we control what URLs will be requested that we don't need to do anything fancy yet so adding a new brute force approach that drops all the smarts at guessing what URLs might be

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,4 +5,9 @@ std::string getUrlContent(const std::string& url);
 
 std::vector<std::string>& splitString(const std::string &s, char delim, std::vector<std::string>& elems);
 
+/**
+  * Check if the given URL falls under the given base; if so then return the 'relative' portion of the URL.
+  */
+std::string urlChildOf(const std::string& url, const std::string& base);
+
 #endif // UTILS_HPP
