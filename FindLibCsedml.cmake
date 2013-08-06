46ecd1257c0a237b6058ed9031aacdf91d05c41f
Hugh Sorby <hsorby@users.noreply.github.com>
2013-08-06 16:21:06 +1200

    Changing the find cmake modules, FindLibCSim  and FindLibCsedml to use FPHSA and follow best practice for CMake modules.

--- FindLibCsedml.cmake
+++ FindLibCsedml.cmake
@@ -1,32 +1,33 @@
 # - Try to find clibsedml
 # Once done, this will define
 #
-#  clibsedml_FOUND - system has libmicrohttpd
-#  clibsedml_INCLUDE_DIRS - the libmicrohttpd include directories
-#  clibsedml_LIBRARIES - link these to use libmicrohttpd
+#  CLIBSEDML_FOUND        - clibsedml was found
+#  CLIBSEDML_INCLUDE_DIRS - the clibsedml include directories
+#  CLIBSEDML_LIBRARIES    - link these to use clibsedml
 
-include(LibFindMacros)
-
-# Dependencies
-#libfind_package(Magick++ Magick)
+#include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(clibsedml_PKGCONF clibsedml)
+#libfind_pkg_check_modules(clibsedml_PKGCONF clibsedml)
 
 # Include dir
-find_path(clibsedml_INCLUDE_DIR
+find_path(CLIBSEDML_INCLUDE_DIR
   NAMES sedml/document.h
-  PATHS ${clibsedml_PKGCONF_INCLUDE_DIRS} /Users/dnic019/shared-folders/resources/my-libs/clibsedml/mac-osx/include
+  PATHS ${clibsedml_PKGCONF_INCLUDE_DIRS}
 )
 
 # Finally the library itself
-find_library(clibsedml_LIBRARY
-  NAMES sedml
-  PATHS ${clibsedml_PKGCONF_LIBRARY_DIRS} /Users/dnic019/shared-folders/resources/my-libs/clibsedml/mac-osx/libs
+find_library(CLIBSEDML_LIBRARY
+  NAMES ${CLIBSEDML_NAMES} sedml
+  PATHS ${clibsedml_PKGCONF_LIBRARY_DIRS}
 )
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(clibsedml_PROCESS_INCLUDES clibsedml_INCLUDE_DIR)
-set(clibsedml_PROCESS_LIBS clibsedml_LIBRARY)
-libfind_process(clibsedml)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cLibsedml CLIBSEDML_LIBRARY CLIBSEDML_INCLUDE_DIR)
+
+if(CLIBSEDML_FOUND)
+  set(CLIBSEDML_INCLUDE_DIRS ${CLIBSEDML_INCLUDE_DIR})
+  set(CLIBSEDML_LIBRARIES ${CLIBSEDML_LIBRARY})
+endif(CLIBSEDML_FOUND)
+
+mark_as_advanced(CLIBSEDML_LIBRARY CLIBSEDML_INCLUDE_DIR)
