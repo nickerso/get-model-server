e4ca7e917a63db462b67049579e7f162be110ab7
Hugh Sorby <h.sorby@auckland.ac.nz>
2013-08-06 16:21:06 +1200

    Changing the find cmake modules, FindLibCSim  and FindLibCsedml to use FPHSA and follow best practice for CMake modules.

--- FindLibCSim.cmake
+++ FindLibCSim.cmake
@@ -1,29 +1,24 @@
 # - Try to find libcsim
 # Once done, this will define
 #
-#  libcsim_FOUND - system has libmicrohttpd
-#  libcsim_INCLUDE_DIRS - the libmicrohttpd include directories
-#  libcsim_LIBRARIES - link these to use libmicrohttpd
-
-include(LibFindMacros)
-
-# Dependencies
-#libfind_package(Magick++ Magick)
+#  LIBCSIM_FOUND - system has libmicrohttpd
+#  LIBCSIM_INCLUDE_DIR - the libmicrohttpd include directories
+#  LIBCSIM_LIBRARIES - link these to use libmicrohttpd
 
 # Include dir
-find_path(libcsim_INCLUDE_DIR
-  NAMES CellmlSimulator.hpp
-  PATHS /Users/dnic019/shared-folders/resources/my-libs/CSim/mac-osx/include
-)
+find_path(LIBCSIM_INCLUDE_DIR CellmlSimulator.hpp)
 
 # Finally the library itself
-find_library(libcsim_LIBRARY
-  NAMES csim
-  PATHS /Users/dnic019/shared-folders/resources/my-libs/CSim/mac-osx/lib
-)
+find_library(LIBCSIM_LIBRARY csim)
 
-# Set the include dir variables and the libraries and let libfind_process do the rest.
-# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(libcsim_PROCESS_INCLUDES libcsim_INCLUDE_DIR)
-set(libcsim_PROCESS_LIBS libcsim_LIBRARY)
-libfind_process(libcsim)
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibCSim LIBCSIM_LIBRARY LIBCSIM_INCLUDE_DIR)
+
+if(LIBCSIM_FOUND)
+  SET(LIBCSIM_INCLUDE_DIRS ${LIBCSIM_INCLUDE_DIR})
+  SET(LIBCSIM_LIBRARIES ${LIBCSIM_LIBRARY})
+endif(LIBCSIM_FOUND)
+
+mark_as_advanced(LIBCSIM_LIBRARY LIBCSIM_INCLUDE_DIR)
+
+
