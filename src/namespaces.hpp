0ebecedecbe5b820138b98d1b70c4c1950e50326
David Nickerson <nickerso@users.sourceforge.net>
2013-08-21 11:58:15 +1200

    properly iterating over the collection of protocols and returning them to the web app as an option array suitable for the select dijit; starting to move all use of namespaces to a predefined list rather than having the strings all over the place

---
+++ src/namespaces.hpp
@@ -0,0 +1,22 @@
+#ifndef NAMESPACES_HPP
+#define NAMESPACES_HPP
+
+#define CELLML_1_1_NS        "http://www.cellml.org/cellml/1.1#"
+#define CELLML_1_0_NS        "http://www.cellml.org/cellml/1.0#"
+#define CMETA_NS             "http://www.cellml.org/metadata/1.0#"
+#define BQS_NS               "http://www.cellml.org/bqs/1.0#"
+#define MATHML_NS            "http://www.w3.org/1998/Math/MathML"
+#define RDF_NS               "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+#define RDF_DATATYPE_DOUBLE  "http://www.w3.org/2001/XMLSchema#double"
+#define RDF_DATATYPE_BOOLEAN "http://www.w3.org/2001/XMLSchema#boolean"
+#define RDF_DATATYPE_INTEGER "http://www.w3.org/2001/XMLSchema#integer"
+#define BQMODEL_NS           "http://biomodels.net/model-qualifiers/"
+#define BQBIO_NS             "http://biomodels.net/biology-qualifiers/"
+#define DC_NS                "http://purl.org/dc/elements/1.1/"
+#define DCTERMS_NS           "http://purl.org/dc/terms/"
+#define VCARD_NS             "http://www.w3.org/2001/vcard-rdf/3.0#"
+#define XML_NS               "http://www.w3.org/XML/1998/namespace"
+
+#define CSIM_NS              "http://cellml.sourceforge.net/csim/metadata/0.1#"
+
+#endif // NAMESPACES_HPP
