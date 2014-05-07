2409896f8aedc81f355f9d244180087c852ed3b4
David Nickerson <nickerso@users.sourceforge.net>
2014-05-07 13:58:44 +1200

    starting to set up the biomaps API

--- src/biomaps.hpp
+++ src/biomaps.hpp
@@ -7,6 +7,14 @@ public:
     Biomaps(const std::string& repositoryRoot);
     ~Biomaps();
 
+    /**
+     * Loads the given model into this manager object. The returned response will contain the unique
+     * identifer to use in future access to this instance of the model.
+     * @param url The URL of the model to load, relative to the known repository root.
+     * @return A JSON object defining the identifier to use when needing access to this model in future calls.
+     */
+    std::string loadModel(const std::string& url);
+
 private:
     Biomaps();
     std::string mRepositoryRoot;
